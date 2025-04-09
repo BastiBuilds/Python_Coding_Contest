c551b64dadd2f33e54bd8b3052387e165b71eb15
BastiBuilds <bastian.bueeler@gmail.com>
2025-04-09 15:26:01 +0200

    Reorganize Order class properties for better readability

--- src/Models/Order.py
+++ src/Models/Order.py
@@ -5,13 +5,14 @@ class Order:
     def __init__(self):
         self.__created_at = datetime.now()
         self.__cones = []
-        @property
-        def created_at(self):
-            return self.__created_at
 
-        @property
-        def cones(self):
-            return self.__cones
+    @property
+    def created_at(self):
+        return self.__created_at
+
+    @property
+    def cones(self):
+        return self.__cones
 
     def add_item(self, item: Cone):
         if not isinstance(item, Cone):
